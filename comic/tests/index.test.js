3c092535f4ef3fed331079246a9d80ef1a1242e0
Nicholas Houdek <nickhoudek@gmail.com>
2023-02-15 21:00:31 -0700

    Added Jest to project

---
+++ comic/tests/index.test.js
@@ -0,0 +1,7 @@
+import Home from "../src/pages/index";
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
+
+test('Fake Test', () => {
+	expect(true).toBeTruthy();
+})
